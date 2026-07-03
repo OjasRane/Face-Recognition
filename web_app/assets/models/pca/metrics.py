f406cf9d291995973ab2cbe007a5161dc32e3648
Ojas Rane <98323556+OjasRane@users.noreply.github.com>
2026-07-03 23:22:14 +0530

    Removed redundant Python script

--- web_app/assets/models/pca/metrics.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay
-
-confusion_matrix = np.load("./confusion_matrix.npy")
-fig, ax = plt.subplots(figsize=(16, 10))
-disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix)
-disp.plot(ax=ax, cmap=plt.cm.Blues)
-plt.show()
