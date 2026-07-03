72c147b2b90fac3c9c4c6ce539787a3478e742da
Ojas Rane <98323556+OjasRane@users.noreply.github.com>
2026-07-03 22:48:18 +0530

    Created Streamlit App for demonstration of face recognition using eigenfaces

---
+++ web_app/landing_page.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Face Recognition",
+    page_icon=":material/familiar_face_and_zone:"
+)
+
+st.title("Face Recognition", text_alignment="center")
+st.markdown("<h2 align='center'>Explore face recognition projects here!</h2>", unsafe_allow_html=True)
+
+with st.container(border=True, key="PCA", horizontal_alignment="center"):
+    st.markdown("<h2 align='center'>Face Recognition using PCA (Eigenfaces)</h2>", unsafe_allow_html=True)
+    st.write("Explore the eigenfaces algorithm for face recognition trained on Olivetti Faces dataset.")
+    if st.button("Explore Now!", key="explore"):
+        st.switch_page("pages/pca.py")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    with st.container(border=True, key="notebooks", horizontal_alignment="center", height=260):
+        st.markdown("<h2 align='center'>Check out the mathematics</h2>", unsafe_allow_html=True)
+        st.text("Check out my notebook on Face Recognition using PCA", text_alignment="center")
+        st.link_button("Face Recognition using PCA",
+                       url="https://OjasRane.github.io/Face-Recognition/face_recognition_using_PCA.html",
+                       icon=":material/import_contacts:")
+
+with col2:
+    with st.container(border=True, key="repo", horizontal_alignment="center", height=260):
+        st.markdown("<h2 align='center'>GitHub Repository</h2>", unsafe_allow_html=True)
+        st.text("Check out the GitHub repository for source code.", text_alignment="center")
+        st.link_button("GitHub Repository", url="https://github.com/OjasRane/Face-Recognition")
+
+with st.container(border=True, height=250, key="connect_with_me", horizontal_alignment="center"):
+    st.markdown("## Connect with me", text_alignment="center")
+    st.image("https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white",
+             link="https://github.com/OjasRane", width=180)
+    st.image("https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white",
+             link="https://linkedin.com/in/ojasrane-in", width=180)
---
+++ web_app/pages/pca.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from utils import N_SUBJECTS, TRAIN_PER_SUBJECT, TEST_PER_SUBJECT, IMG_SIZE, IMG_SHAPE
+import joblib
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.set_page_config(
+    page_title="Face Recognition | Eigenfaces",
+    page_icon=":material/familiar_face_and_zone:"
+)
+
+@st.cache_resource
+def load(path: str):
+    if path[-3:] == "npz":
+        with np.load(path) as dataset:
+            return dataset["train_faces"], dataset["test_faces"]
+    elif path[-3:] == "pkl":
+        payload = joblib.load(path)
+        return payload["model"], payload["projected"]
+    elif path[-3:] == "png":
+        return plt.imread(path)
+    else:
+        raise ValueError("Expected path to either npz or pkl file.")
+
+def find_match(test_image_vector, model, projection):
+    test_image_vector = test_image_vector.reshape(-1, IMG_SIZE)
+    projection_of_test_image = model.transform(test_image_vector)
+    distance = np.linalg.norm(projection - projection_of_test_image, axis=1)
+    min_distance_idx = np.argmin(distance)
+    return min_distance_idx, distance[min_distance_idx]
+
+st.title("Face Recognition using Eigenfaces", text_alignment="center")
+st.markdown("<h3 align='center'>Check the confusion matrix below to find cases where the model fails.</h3>", unsafe_allow_html=True)
+
+st.selectbox("Select subject", options=[f"Subject {i}" for i in range(N_SUBJECTS)], key="subject")
+st.pills("Select Image", options=[f"Image {i}" for i in range(TEST_PER_SUBJECT)], default="Image 0", required=True, key="image")
+st.session_state["test_face_idx"] = TEST_PER_SUBJECT*int(st.session_state["subject"].split(" ")[-1]) + int(st.session_state["image"].split(" ")[-1])
+
+pca, projection = load("web_app/assets/models/pca/pca.pkl")
+train_faces, test_faces = load("web_app/assets/datasets/olivetti_faces_dataset.npz")
+
+col1, col2 = st.columns(2)
+with col1:
+    fig, ax = plt.subplots()
+    ax.imshow(test_faces[st.session_state["test_face_idx"]].reshape(IMG_SHAPE), cmap="gray", interpolation="bilinear")
+    ax.set_title(f"{st.session_state['image']} of {st.session_state['subject']}")
+    ax.set_axis_off()
+    st.pyplot(fig)
+    plt.close(fig)
+with col2:
+    min_distance_idx, distance = find_match(test_faces[st.session_state["test_face_idx"]], pca, projection)
+    fig, ax = plt.subplots()
+    ax.imshow(train_faces[min_distance_idx].reshape(IMG_SHAPE), cmap="gray", interpolation="bilinear")
+    ax.set_title(f"Image of subject {min_distance_idx // TRAIN_PER_SUBJECT}")
+    ax.set_axis_off()
+    st.pyplot(fig)
+    plt.close(fig)
+
+if int(st.session_state["subject"].split(" ")[-1]) == min_distance_idx // TRAIN_PER_SUBJECT:
+    st.markdown("<p align='center' style='font-size: 24px;'>Prediction is correct!</p>", unsafe_allow_html=True)
+else:
+    st.markdown("<p align='center' style='font-size: 24px;'>Prediction is incorrect!</p>", unsafe_allow_html=True)
+st.markdown(f"<p align='center' style='font-size: 24px;'>Distance between projection of both images: {np.round(distance, 4)} units</p>", unsafe_allow_html=True)
+
+st.markdown("<h3 align='center'>Confusion Matrix:</h3>", unsafe_allow_html=True)
+confusion_matrix = load("web_app/assets/models/pca/confusion_matrix.png")
+st.image(confusion_matrix)
+
+if st.button("Home", icon=":material/home:"):
+    st.switch_page("landing_page.py")
